bdcf0a926a8ef5023fc08b002ab08af47be48b02
amritregmi26 <amrregmi@gmail.com>
2023-11-13 14:25:39 +0545

    singly linked list with empty, insert at first and insert at last method has been added

--- Linked List/singlyLinkedList.js
+++ Linked List/singlyLinkedList.js
@@ -1,1 +1,45 @@
-// File will be updated later
+class Node
+{
+    constructor(data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList
+{
+    constructor()
+    {
+        this.head = null;
+    }
+
+    isEmpty()
+    {
+        return this.head === null;
+    }
+
+    insertLast(data)
+    {
+        const newNode = new Node(data);
+        if(!this.head)
+        {
+            this.head = newNode;
+        }
+        let current = this.head;
+        while(current.next)
+        {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    insertFirst(data)
+    {
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+     
+}
