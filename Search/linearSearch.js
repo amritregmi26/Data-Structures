bb154dca7e49dbb4237806ceeb7194b3ce41ea1d
amritregmi26 <amrregmi@gmail.com>
2023-12-01 15:28:30 +0545

    Linear Search implemented

---
+++ Search/linearSearch.js
@@ -0,0 +1,11 @@
+const linearSearch = (arr, target) => {
+    for(let i = 0; i < arr.length; i++)
+    {
+        if(arr[i] === target)
+        {
+            return i
+        }
+    }
+
+    return null;
+}
